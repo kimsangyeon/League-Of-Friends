7398a3ec5ebe5562c22fa17b89dd191611ce1497
dho113@naver.com <dho113@naver.com>
2021-08-31 20:41:46 +0900

    feature/init-config - init config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+
+  images: {
+    domains: ['ddragon.leagueoflegends.com'],
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: '/lol/:slug*',
+        destination: 'https://kr.api.riotgames.com/lol/:slug*',
+      }
+    ]
+  },
 }
