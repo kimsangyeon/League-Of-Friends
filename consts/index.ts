8666cf16e44d94d1fb1275d274f373c8ca6221fd
dho113@naver.com <dho113@naver.com>
2021-08-31 20:44:44 +0900

    feature/init-profile - consts 상수값 생성 및 정의

---
+++ consts/index.ts
@@ -0,0 +1,11 @@
+export const GET_SUMMONER_BY_NAME_URL = '/lol/summoner/v4/summoners/by-name';
+export const GET_SUMMONER_RANK_BY_ID_URL = '/lol/league/v4/entries/by-summoner'
+
+export const PROFILE_ICON_URL = 'http://ddragon.leagueoflegends.com/cdn/11.16.1/img/profileicon';
+
+export const RIOT_KEY = '';
+
+export const RANK_TO_MAP: {[key: string]: string} = {
+  'RANKED_SOLO_5x5': '솔로랭크',
+  'RANKED_FLEX_SR': '자유 5:5 랭크'
+};
