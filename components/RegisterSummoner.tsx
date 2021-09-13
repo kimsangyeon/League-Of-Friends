ea02640ca2d46615ee977f2e9b78da5430966de4
dho113@naver.com <dho113@naver.com>
2021-09-13 22:18:33 +0900

    feature/lead-components - RegisterSummoner css 및 버튼 이미지 변경

--- components/RegisterSummoner.tsx
+++ components/RegisterSummoner.tsx
@@ -1,7 +1,9 @@
-import { checkExistSummoner } from "@hooks/summoner";
-import { getLocalStorageByNameList, setLocalStorageByNameList } from "@utils/storageUtils";
-import { MouseEvent, useState } from "react";
-import { useQueryClient } from "react-query";
+import { MouseEvent, useState } from 'react';
+import Image from 'next/image';
+import { useQueryClient } from 'react-query';
+import styles from '@styles/common.module.css';
+import { checkExistSummoner } from '@hooks/summoner';
+import { getLocalStorageByNameList, setLocalStorageByNameList } from '@utils/storageUtils';
 
 const RegisterSummoner = () => {
   const queryClient = useQueryClient();
@@ -18,9 +20,16 @@ const RegisterSummoner = () => {
   };
 
   return (
-    <div>
-      <input value={summonerName} onChange={(e) => setSummonerName(e.target.value)} />
-      <button onClick={onRegister}>추가</button>
+    <div className={styles.registerWrap}>
+      <input
+        className={styles.registerInput}
+        value={summonerName}
+        placeholder="소환사명, 소환사명, ..."
+        onChange={(e) => setSummonerName(e.target.value)}
+      />
+      <button className={styles.registerButton} onClick={onRegister}>
+        <Image src="/images/icon/search.png" alt="search_icon" width="30" height="30" />
+      </button>
     </div>
   )
 };
