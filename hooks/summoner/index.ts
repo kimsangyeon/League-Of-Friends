1fa2f9a0f790b7a439ef4aad9f2d104510e2a14b
dho113@naver.com <dho113@naver.com>
2022-03-16 22:57:36 +0900

    feature: switch swr - useRank 작업

--- hooks/summoner/index.ts
+++ hooks/summoner/index.ts
@@ -1,7 +1,6 @@
-import { useQuery } from 'react-query';
 import { apiGet } from '@utils/apiUtils';
-import { GET_SUMMONER_BY_NAME_URL, GET_SUMMONER_RANK_BY_ID_URL, API_SUMMONER_PREFIX_FOR_SERVER } from '@consts/index';
-import { RankInfo, SummonerInfo } from '@models/summoner';
+import { GET_SUMMONER_BY_NAME_URL, API_SUMMONER_PREFIX_FOR_SERVER } from '@consts/index';
+import { SummonerInfo } from '@models/summoner';
 
 export const fetchSummoner = async (summonerName = '', isServer = false) => {
   if (!summonerName) return null;
@@ -22,18 +21,3 @@ export const checkExistSummoner = async (summonerName = '') => {
     console.warn(e);
   }
 };
-
-export const fetchRank = async (summonerId = '', isServer = false) => {
-  if (!summonerId) return null;
-
-  return await apiGet<RankInfo[]>(`${isServer ? API_SUMMONER_PREFIX_FOR_SERVER : ''}${GET_SUMMONER_RANK_BY_ID_URL}/${summonerId}`);
-}
-
-export const useRank = (summonerId = ''): { rank: RankInfo[] | undefined; isRankLoading: boolean; isRankFetching: boolean} => {
-  const { data: rankData, isLoading: isRankLoading, isFetching: isRankFetching } = useQuery(['rank', summonerId], () => fetchRank(summonerId));
-  return {
-    rank: rankData?.data,
-    isRankLoading,
-    isRankFetching,
-  };
-}
